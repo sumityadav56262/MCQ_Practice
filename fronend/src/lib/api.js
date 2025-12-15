c3b58a044151e9299b6fea2abe5b2efbc7806d57
sumityadav56262 <sumityadav56262@gmail.com>
2025-12-15 23:20:19 +0530

    added env production

--- fronend/src/lib/api.js
+++ fronend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: VITE_API_BASE_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
